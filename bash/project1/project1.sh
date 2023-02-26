b796c7e23e07ac440bb21c6a987a6aad94dee7ed
mcdonotj <122412604+mcdonotj@users.noreply.github.com>
2023-02-26 18:13:40 -0500

    Add files via upload

---
+++ bash/project1/project1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+my_atk=$((RANDOM % 20 + 1))
+echo "$atk"
+ if [ "$my_atk" > 18 ]; then
+	echo "$atk is greater than 18, you hit the enemy!"
+elif [ "$my_atk" = 18 ]; then
+	echo "$atk is equal to 18, you hit the enemy!"
+else
+	echo "$atk is less than 18, your attack misses..."
+fi
