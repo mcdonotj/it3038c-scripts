eb27563885d3e0e6af7b269eeefe5ae31e737b3f
mcdonotj <122412604+mcdonotj@users.noreply.github.com>
2023-02-26 21:50:22 -0500

    Update project1.sh

--- bash/project1/project1.sh
+++ bash/project1/project1.sh
@@ -4,9 +4,9 @@
 my_atk=$((RANDOM % 20 + 1))
 echo "$atk"
  if [ "$my_atk" > 18 ]; then
-	echo "$atk is greater than 18, you hit the enemy!"
+	echo "$my_atk is greater than 18, you hit the enemy!"
 elif [ "$my_atk" = 18 ]; then
-	echo "$atk is equal to 18, you hit the enemy!"
+	echo "$my_atk is equal to 18, you hit the enemy!"
 else
-	echo "$atk is less than 18, your attack misses..."
+	echo "$my_atk is less than 18, your attack misses..."
 fi
